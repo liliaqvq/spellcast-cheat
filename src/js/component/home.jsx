05ea2ce6686cf7d08ce60f14db3f96f0146fec40
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-12-27 19:47:19 +0000

    started tiles distribution

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,25 +2,62 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import '../../styles/index.css'
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+    return (
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '10px'}}>
+			<div>
+				<h2 style={{ color: 'white' }}>SPELLCAST CHEAT</h2>
+			</div>
+			<div className="board">
+				<div className="small-square">
+					<div className="row mt-2">
+						<div className="col ms-2">
+							<div className="tile">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+						</div>
+	
+						<div className="col">
+							<div className="tile">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+						</div>
+	
+						<div className="col">
+							<div className="tile">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+						</div>
+	
+						<div className="col">
+							<div className="tile">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+						</div>
+	
+						<div className="col me-2">
+							<div className="tile">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+							<div className="tile mt-2">A</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
-};
+};	
 
 export default Home;
