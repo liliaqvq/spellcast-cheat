ce70bff598a7d9803aae0339e15e557f744ee1da
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-12-28 19:30:30 +0000

    input tiles and clear all

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,58 +6,62 @@ import '../../styles/index.css'
 
 //create your first component
 const Home = () => {
-    return (
-		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '10px'}}>
+	const inputRefs = Array.from({ length: 25 }, () => useRef(null));
+
+    const handleInputChange = (index, event) => {
+        const input = event.target;
+        const value = input.value;
+
+		// Convert to uppercase
+        const uppercaseValue = value.toUpperCase();
+
+        // Update the input value
+        input.value = uppercaseValue;
+
+        // Move to the next input if the current one is filled
+        if (value.length === 1 && index < inputRefs.length - 1) {
+            inputRefs[index + 1].current.focus();
+        }
+    };
+
+	const handleClearAll = () => {
+        // Clear all input values
+        inputRefs.forEach((ref) => {
+            if (ref.current) {
+                ref.current.value = "";
+            }
+        });
+
+        // Set focus on the first input
+        if (inputRefs[0].current) {
+            inputRefs[0].current.focus();
+        }
+    };
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '20px' }}>
 			<div>
 				<h2 style={{ color: 'white' }}>SPELLCAST CHEAT</h2>
 			</div>
 			<div className="board">
+				<button className="clear-button" onClick={handleClearAll}>Clear All</button>
 				<div className="small-square">
-					<div className="row mt-2">
-						<div className="col ms-2">
-							<div className="tile">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-						</div>
-	
-						<div className="col">
-							<div className="tile">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-						</div>
-	
-						<div className="col">
-							<div className="tile">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-						</div>
-	
-						<div className="col">
-							<div className="tile">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-						</div>
-	
-						<div className="col me-2">
-							<div className="tile">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-							<div className="tile mt-2">A</div>
-						</div>
+					<div className="matrix-container content-center">
+						{[...Array(25).keys()].map(index => (
+							<input 
+								key={index} 
+								type="text" 
+								className={`matrix-tile number${index + 1}`} 
+								maxLength="1" 
+								ref={inputRefs[index]}
+                                onChange={(event) => handleInputChange(index, event)}
+							/>
+						))}
 					</div>
 				</div>
 			</div>
 		</div>
 	);
-};	
+};
 
 export default Home;
